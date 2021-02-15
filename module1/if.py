85623aa7d695451e4d976f6d0c95a020047256ba
Vlad <Vlad.grrr@gmail.com>
2021-02-15 18:04:44 +0300

    if2

--- module1/if.py
+++ module1/if.py
@@ -10,11 +10,21 @@ tourists = [{'user': users[0], 'city': cities[2]},
 
 city = input('Введите город: ')
 
-if city.lower() == cities[0].lower():
-    print(f"Турист {tourists[1]['user']['name']} возраст {tourists[1]['user']['age']}. Посетил город {city.lower()}")
+try:
+    city[0]
+    if city not in cities:
+        print('Такого города нет в списке')
 
-if city.lower() == cities[1].lower():
-    print(f"Турист {tourists[2]['user']['name']} возраст {tourists[2]['user']['age']}. Посетил город {city.lower()}")
 
-if city.lower() == cities[2].lower():
-    print(f"Турист {tourists[0]['user']['name']} возраст {tourists[0]['user']['age']}. Посетил город {city.lower()}")
+    if city.lower() == cities[0].lower():
+        print(f"Турист {tourists[1]['user']['name']} возраст {tourists[1]['user']['age']}. Посетил город {city.lower()}")
+
+    if city.lower() == cities[1].lower():
+        print( f"Турист {tourists[2]['user']['name']} возраст {tourists[2]['user']['age']}. Посетил город {city.lower()}")
+
+    if city.lower() == cities[2].lower():
+        print(f"Турист {tourists[0]['user']['name']} возраст {tourists[0]['user']['age']}. Посетил город {city.lower()}")
+
+
+except IndexError:
+    print('Вы не указали город')
