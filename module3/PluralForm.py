da3362259a8e9eb375bd4f3b717cafb1dc704c10
Vlad <Vlad.grrr@gmail.com>
2021-02-19 16:40:53 +0300

    Plural_form

--- module3/PluralForm.py
+++ module3/PluralForm.py
@@ -1,16 +1,19 @@
-number = 0
-form1 = ''
-form2 = ''
-form3 = ''
-
 def plural_form(number, form1, form2, form3):
-    if number == 1:
+    """Функуия согласования окончаний существительных в зависимости от переданного числа
+    :param number: число, к которому необходимо подобрать форму существительного
+    :param form1: 1-я форма существительного
+    :param form2: 2-я форма существительного
+    :param form3: 3-я форма существительного"""
+    if  number == 11 or number == 12 or number == 13 or number == 14 or number == 15 or number == 16 or number == 17 or number == 18 or number == 19:
+         print(f"{number} {form3}")
+
+    elif number == 1 or number % 10 == 1:
         print(f"{number} {form1}")
-    
-    if number >= 2 and number <= 4:
+
+    elif number >= 2 and number <= 4 or number % 10 == 2 or number % 10 == 3 or number % 10 == 4:
         print(f"{number} {form2}")
     
-    if number >= 5:
+    elif number >= 5  or number % 10 == 6 or number % 10 == 7 or number % 10 == 8 or number % 10 == 9 or number % 10 == 0:
         print(f"{number} {form3}")
 
 plural_form(1, 'яблоко', 'яблока', 'яблок') 
